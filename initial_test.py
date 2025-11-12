7df114c765ab918e8ea66e678c6a351096d24521
ThomasTorres2024 <thomastorres2024@gmail.com>
2025-11-11 22:44:25 -0500

    added local map paper and local map github stuff

---
+++ initial_test.py
@@ -0,0 +1,21 @@
+from pacmap import LocalMAP
+import numpy as np
+import matplotlib.pyplot as plt
+
+# loading preprocessed coil_20 dataset
+# you can change it with any dataset that is in the ndarray format, with the shape (N, D)
+# where N is the number of samples and D is the dimension of each sample
+X = np.load("./data/coil_20.npy", allow_pickle=True)
+X = X.reshape(X.shape[0], -1)
+y = np.load("./data/coil_20_labels.npy", allow_pickle=True)
+
+# initializing the pacmap instance
+# Setting n_neighbors to "None" leads to an automatic choice shown below in "parameter" section
+embedding = LocalMAP(n_components=2, n_neighbors=10, MN_ratio=0.5, FP_ratio=2.0) 
+
+# fit the data (The index of transformed data corresponds to the index of the original data)
+X_transformed = embedding.fit_transform(X, init="pca")
+
+# visualize the embedding
+fig, ax = plt.subplots(1, 1, figsize=(6, 6))
+ax.scatter(X_transformed[:, 0], X_transformed[:, 1], cmap="Spectral", c=y, s=0.6)
